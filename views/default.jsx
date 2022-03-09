72f9ef469c14402ee4419cdf67e86d4a216d461a
unknown <ramiropen.work@gmail.com>
2022-03-08 20:52:05 -0800

    adding default view for layout

---
+++ views/default.jsx
@@ -0,0 +1,22 @@
+// const React = require('react')
+// ------------------------------
+// const React = require('react')
+
+// function Def (html) {}
+// --------------------------------
+const React = require('react')
+
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+  }
+
+module.exports = Def
