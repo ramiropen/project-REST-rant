a99dcfce05710c5c70e45be7479c98d7357a8756
unknown <ramiropen.work@gmail.com>
2022-03-10 19:46:57 -0800

    adding css

--- index.js
+++ index.js
@@ -59,21 +59,49 @@
 // ----------------------------------------------------------
 
 // step 5
-require("dotenv").config();
-const express = require("express");
-const app = express();
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
 
-app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views").createEngine());
+// app.set("view engine", "jsx");
+// app.engine("jsx", require("express-react-views").createEngine());
 
-app.use("/places", require("./controllers/places"));
+// app.use("/places", require("./controllers/places"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// app.get("/", (req, res) => {
+//   res.render("home");
+// });
 
-app.get("*", (req, res) => {
-  res.render("error404");
-});
+// app.get("*", (req, res) => {
+//   res.render("error404");
+// });
 
-app.listen(process.env.PORT);
+// app.listen(process.env.PORT);
+// ------------------------------------------------
+
+// part 6
+
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
+// Controllers & Routes
+app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('*', (req, res) => {
+    res.render('error404')
+})
+
+// Listen for Connections
+app.listen(process.env.PORT)
--- views/home.jsx
+++ views/home.jsx
@@ -10,9 +10,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <a href="/places"><button className="btn-primary">Places Page</button></a>
           </main>
       </Def>
     )
   }
+
+  
 
 module.exports = home
