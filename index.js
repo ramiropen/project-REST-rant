8ad6eaabccffe35d26c285e6a4fd62331eee9a59
unknown <ramiropen.work@gmail.com>
2022-03-10 21:19:45 -0800

    finalized

--- index.js
+++ index.js
@@ -82,26 +82,26 @@
 // part 6
 
 // Modules and Globals
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
 // Express Settings
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
 
 // Controllers & Routes
-app.use('/places', require('./controllers/places'))
+app.use("/places", require("./controllers/places"));
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('*', (req, res) => {
-    res.render('error404')
-})
+app.get("*", (req, res) => {
+  res.render("error404");
+});
 
 // Listen for Connections
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
